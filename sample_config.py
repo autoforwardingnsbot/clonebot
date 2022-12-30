544729ac781fcf53a628d1c9feafd7aa687cb422
autoforwardingnsbot <109038679+autoforwardingnsbot@users.noreply.github.com>
2022-12-30 21:01:30 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -12,19 +12,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5989527178:AAGE13kpv859YYecSJr_wSL3oARV0I9DFUM")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "17718356"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "fe2dec8e960bf8dcb4e4820d81fe7d46")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQCf3ZJRphYVxJOGlvDIY29BPwTKR9RKYxJgJX_gNqDLZPA80z_9R1qOuA1aTbDxU7RomZXwDh4_6SC6fux7PGn_mLsVjX-m6xuV3uXPK4r4XRNVwvpv9O8RnDyMMWr8UsXX8dO5T3MgepIPa0RZCQRQ94KccoobX_VEjv2hzgeQ3t-skRwZZLTTJpa-NFN3_mrfLNcB8IA7HTMNi4nlzT5Ljbom-dtlHbQoM9Bau3ozyW16DgCYhrss3DwERrWoi022YK_0iOqpvLn0mBEwr3Eox9oinZHbLIk792XOyMX5lkh9526C1JIj-SdayRkpvtRzXNYpFoFWAMkqWTjxUHk0AAAAAVre2SwA")
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://amritsinghbaba01:1998@cluster0.3tacgfj.mongodb.net/?retryWrites=true&w=majority")
 
 
 def LOGGER(name: str) -> logging.Logger:
