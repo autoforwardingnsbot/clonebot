9a4fe37d0b26a608bdec1a6441b213674928e359
autoforwardingnsbot <109038679+autoforwardingnsbot@users.noreply.github.com>
2022-12-30 21:04:58 +0530

    Update sample-vps-config.py

--- sample-vps-config.py
+++ sample-vps-config.py
@@ -10,11 +10,11 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
 
-    TG_BOT_TOKEN = ""
-    APP_ID = 
-    API_HASH = ""
-    TG_USER_SESSION = ""
-    DB_URI = ""
+    TG_BOT_TOKEN = "5989527178:AAGE13kpv859YYecSJr_wSL3oARV0I9DFUM"
+    APP_ID = "17718356"
+    API_HASH = "fe2dec8e960bf8dcb4e4820d81fe7d46"
+    TG_USER_SESSION = "BQCf3ZJRphYVxJOGlvDIY29BPwTKR9RKYxJgJX_gNqDLZPA80z_9R1qOuA1aTbDxU7RomZXwDh4_6SC6fux7PGn_mLsVjX-m6xuV3uXPK4r4XRNVwvpv9O8RnDyMMWr8UsXX8dO5T3MgepIPa0RZCQRQ94KccoobX_VEjv2hzgeQ3t-skRwZZLTTJpa-NFN3_mrfLNcB8IA7HTMNi4nlzT5Ljbom-dtlHbQoM9Bau3ozyW16DgCYhrss3DwERrWoi022YK_0iOqpvLn0mBEwr3Eox9oinZHbLIk792XOyMX5lkh9526C1JIj-SdayRkpvtRzXNYpFoFWAMkqWTjxUHk0AAAAAVre2SwA"
+    DB_URI = "mongodb+srv://amritsinghbaba01:1998@cluster0.3tacgfj.mongodb.net/?retryWrites=true&w=majority"
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
